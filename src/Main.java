953f4df65ad40ebe38c0f6120c6eb1ca16ce44cf
Noah8282 <nova0001@stud.kea.dk>
2024-02-21 10:44:46 +0100

    Filmsamlingsprojekt V1

--- src/Main.java
+++ src/Main.java
@@ -11,19 +11,32 @@ public class Main {
             System.out.println("Velkommen til filmsamling!: Tryk følgende:\n" +
                     "Add: Tilføj film\n" +
                     "List: Se liste over film\n" +
+                    "Search: Søg på en film\n" +
                     "Exit: Stop programmet");
             input = scanner.nextLine();
             if(input.equalsIgnoreCase("add")) {
                 System.out.println("Type information of movie as follows. Remember commas (,): Title, Director, Genre, Year, Length in Minutes, Movie in color? (Type true if in color. Type false if black and white)");
 
-                String movieString = scanner.nextLine().trim();
-                String[] movieAttributes = movieString.replaceAll("\\s*,\\s*", ",").split(",");
-
-                movieCollection.addMovie(movieAttributes[0],movieAttributes[1],movieAttributes[2],Integer.parseInt(movieAttributes[3]),Integer.parseInt(movieAttributes[4]),Boolean.parseBoolean(movieAttributes[5]));
+                movieCollection.addMovie(scanner.nextLine());
             }
             if(input.equalsIgnoreCase("list")) {
                 System.out.println(movieCollection.getList());;
             }
+            if(input.equalsIgnoreCase("search")) {
+                System.out.println("Which category do you want to search in? (Title, Director, Genre, Year, Minutes (length in minutes), Colored");
+                String searchElement = scanner.next();
+                scanner.nextLine();
+                System.out.println("Write what you want to search for:");
+                String searchString = scanner.next();
+                scanner.nextLine();
+
+                try {
+                    System.out.println(movieCollection.search(searchElement, searchString));
+                } catch (Exception e) {
+                    System.out.println("Invalid search element: " + searchElement);
+                }
+            }
+            System.out.println("\n\n\n");
         }while(!input.equalsIgnoreCase(SENTINAL));
 
 
--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,9 +1,8 @@
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.function.Function;
 
 public class MovieCollection {
-    private StringBuilder list = new StringBuilder();
 
     private List<Movie> movieCollection;
 
@@ -13,30 +12,88 @@ public class MovieCollection {
     }
 
 
+    public void addMovie(String str) {
 
-    public void addMovie(String title, String director, String genre, int yearCreated, int lengthInMinutes, boolean isInColor) {
-        movieCollection.add(new Movie(title, director, genre, yearCreated, lengthInMinutes, isInColor));
+        String[] movieAttributes = str.trim().replaceAll("\\s*,\\s*", ",").split(",");
+
+        if(movieAttributes.length == 6) {
+            movieCollection.add(new Movie(movieAttributes[0],movieAttributes[1],movieAttributes[2],Integer.parseInt(movieAttributes[3]),Integer.parseInt(movieAttributes[4]),Boolean.parseBoolean(movieAttributes[5])));
+        } else {
+            System.out.println("All requested inputs was not entered correctly.");
+        }
     }
 
     public String getList() {
-
+        StringBuilder list = new StringBuilder();
         movieCollection.forEach(m -> {
             list.append(
                     "################\n" +
-                    "Title: "+m.getTitle()+"\n"+
-                    "Director: "+m.getDirector()+"\n"+
-                    "Genre: "+m.getGenre()+"\n"+
-                    "Year: "+ m.getYearCreated()+"\n"+
-                    "Length: "+m.getLengthInMinutes()+" min\n"+
-                    "Color: "+((m.isInColor()) ? "Colored" : "Black/White")+"\n"+
-                    "################\n");
+                            "Title: " + m.getTitle() + "\n" +
+                            "Director: " + m.getDirector() + "\n" +
+                            "Genre: " + m.getGenre() + "\n" +
+                            "Year: " + m.getYearCreated() + "\n" +
+                            "Length: " + m.getLengthInMinutes() + " min\n" +
+                            "Color: " + ((m.isInColor()) ? "Colored" : "Black/White") + "\n" +
+                            "################\n");
         });
 
         return list.toString();
     }
 
-    public String search(int index) {
+    public String search(String searchElement, String searchString) {
 
+        List<Movie> matchingElements = searchHelper(searchElement, searchString);
+
+        if (!matchingElements.isEmpty()) {
+            String returnString = "##### Search results #####\n";
+            returnString += convertToMovieList(matchingElements);
+            return returnString;
+        }
+        return "##### No matching search results #####";
     }
 
+
+
+
+
+    //Helper methods
+
+    private List<Movie> searchHelper(String searchElement, String searchString) {
+        Function<Movie, String> getter = switch (searchElement.toLowerCase()) {
+            case "title" -> Movie::getTitle;
+            case "director" -> Movie::getDirector;
+            case "genre" -> Movie::getGenre;
+            case "year" -> Movie -> String.valueOf(Movie.getYearCreated());
+            case "minutes" -> Movie -> String.valueOf(Movie.getLengthInMinutes());
+            case "colored" -> Movie -> String.valueOf(Movie.isInColor());
+            default -> throw new IllegalArgumentException("Invalid search element: " + searchElement);
+        };
+
+        List<Movie> matchingElements = movieCollection.stream()
+                .filter(movie -> getter.apply(movie).toLowerCase().trim().contains(searchString.toLowerCase()))
+                .toList();
+
+        return matchingElements;
+    }
+
+    private String convertToMovieList(List<Movie> list) {
+        StringBuilder returnElements = new StringBuilder();
+        int count = 1;
+        for(Movie m : list) {
+            returnElements.append("##### Movie "+ (count++) +" #######\n" +
+                    "Title: " + m.getTitle() + "\n" +
+                    "Director: " + m.getDirector() + "\n" +
+                    "Genre: " + m.getGenre() + "\n" +
+                    "Year: " + m.getYearCreated() + "\n" +
+                    "Length: " + m.getLengthInMinutes() + " min\n" +
+                    "Color: " + ((m.isInColor()) ? "Colored" : "Black/White") + "\n" +
+                    "################\n");
+        }
+
+        return returnElements.toString();
+    }
+
+
 }
+
+
